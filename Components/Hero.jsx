dea86ef556e9f0b61b310411ced224cef05c3b42
Mahesh Patel <mp8148515@gmail.com>
2023-01-27 11:06:39 +0530

    header complete

---
+++ Components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className="border-2 border-red-900 min-h-screen flex justify-center items-center ">
+
+        </div>
+    )
+}
+
+export default Hero
---
+++ Components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import img from "../public/newImages.jpg"
+import Image from 'next/image'
+import { AiOutlineMail, AiFillGithub, AiOutlineInstagram, AiOutlineLinkedin } from "react-icons/ai"
+import { motion } from "framer-motion"
+const Navbar = () => {
+    return (
+        <header className=" grid sm:grid-cols-1 md:grid-cols-2 p-2">
+            <motion.div
+                initial={{
+                    x: -500,
+                    opacity: 0.5
+                }}
+                transition={{
+                    duration: 1.1
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1
+                }}
+                className="left-section flex justify-center items-center">
+                <Image src={img} className="w-20 h-20 rounded-full" />
+            </motion.div>
+            <motion.div
+                initial={{
+                    y: -200,
+                    opacity: 0.5
+                }}
+                transition={{
+                    duration: 1.1
+                }}
+                animate={{
+                    y: 0,
+                    opacity: 1
+                }}
+                className="right-section  flex justify-evenly items-center">
+                <span><AiOutlineMail size={30} /></span>
+                <span><AiFillGithub size={30} /></span>
+                <span><AiOutlineInstagram size={30} /></span>
+                <span><AiOutlineLinkedin size={30} /></span>
+            </motion.div>
+        </header>
+    )
+}
+
+export default Navbar
